c7c7f748cffca22458e028a7e2563f23147714c9
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-08-05 09:46:18 +0530

    tagging the bot

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const { Client } = require('whatsapp-web.js');
+const { Client,LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client(
+    {
+        authStrategy: new LocalAuth(),
+    }
+);
 
 client.once('ready', () => {
     console.log('Client is ready!');
+    //console.log(client.info)
 });
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
 });
+
+client.on('message', async (message) => {
+    if (!message.from.includes('@g.us')) {
+        return;
+    }
+    const botId = client.info.wid._serialized;
+    console.log(botId);
+    const mentionedIds = message.mentionedIds || [];
+    if(mentionedIds.includes(botId)){
+        console.log('Bot was mentioned in the group');
+        console.log(`Message: ${message.body}`);
+        await message.reply('Hey you mentioned me!')
+
+    }
+
+
+})
 
 client.initialize();
 
